7600cfa479cc8796ce1ca41ed356fc14be311599
Kelvin Lee <kelvin.lee@lansa.com.au>
2016-08-25 17:37:08 +1000

    added tests

--- tests/check-stateful.bat
+++ tests/check-stateful.bat
@@ -2,8 +2,12 @@
 REM Simple check of a stateful encoding.
 REM Usage: check-stateful.bat SRCDIR CHARSET
 
-..\src\iconv_no_i18n -f %2 -t UTF-8 < %1\%2-snippet > tmp-snippet
+if not .%TESTBIN%. == .. goto :start
+set TESTBIN=.
+:start
+
+%TESTBIN%\iconv -f %2 -t UTF-8 < %1\%2-snippet > tmp-snippet
 fc %1\%2-snippet.UTF-8 tmp-snippet
-..\src\iconv_no_i18n -f UTF-8 -t %2 < %1\%2-snippet.UTF-8 > tmp-snippet
+%TESTBIN%\iconv -f UTF-8 -t %2 < %1\%2-snippet.UTF-8 > tmp-snippet
 fc %1\%2-snippet tmp-snippet
 del tmp-snippet
--- tests/check-translit.bat
+++ tests/check-translit.bat
@@ -2,6 +2,10 @@
 REM Simple check of transliteration facilities.
 REM Usage: check-translit.bat SRCDIR FILE FROMCODE TOCODE
 
-..\src\iconv_no_i18n -f %3 -t %4//TRANSLIT < %1\%2.%3 > tmp
+if not .%TESTBIN%. == .. goto :start
+set TESTBIN=.
+:start
+
+%TESTBIN%\iconv_no_i18n -f %3 -t %4//TRANSLIT < %1\%2.%3 > tmp
 fc %1\%2.%4 tmp
 del tmp
