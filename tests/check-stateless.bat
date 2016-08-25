4ab4e4a39fa958a64ca3716993f133879be5acc3
Kelvin Lee <kelvin.lee@lansa.com.au>
2016-08-25 17:30:02 +1000

    added tests

--- tests/check-stateless.bat
+++ tests/check-stateless.bat
@@ -2,13 +2,17 @@
 REM Complete check of a stateless encoding.
 REM Usage: check-stateless.bat SRCDIR CHARSET
 
-.\table-from %2 > tmp-%2.TXT
-.\table-to %2 | sort > tmp-%2.INVERSE.TXT
+if not .%TESTBIN%. == .. goto :start
+set TESTBIN=.
+:start
+
+%TESTBIN%\table-from %2 > tmp-%2.TXT
+%TESTBIN%\table-to %2 | sort > tmp-%2.INVERSE.TXT
 fc %1\%2.TXT tmp-%2.TXT
 
 if not exist %1\%2.IRREVERSIBLE.TXT goto ELSE_1
   copy /a %1\%2.TXT /a + %1\%2.IRREVERSIBLE.TXT /a tmp
-  sort < tmp | uniq-u > tmp-orig-%2.INVERSE.TXT
+  sort < tmp | %TESTBIN%\uniq-u > tmp-orig-%2.INVERSE.TXT
   fc tmp-orig-%2.INVERSE.TXT tmp-%2.INVERSE.TXT
   del tmp
   del tmp-orig-%2.INVERSE.TXT
